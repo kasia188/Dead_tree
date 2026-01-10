e73f45ddf29dc10bed985925d4208e56281baed2
kasia188 <115277@g.elearn.uz.zgora.pl>
2026-01-11 00:10:34 +0100

    Adjusting defs to config

--- src/segmentation.py
+++ src/segmentation.py
@@ -8,7 +8,7 @@ from processing import clean_mask, remove_big_objects, adaptive_fp_cleanup
 
 # Generates segmentation masks by combining red, blue and hue channel thresholds with NIR,
 # performs multiple cleaning operations, and stores intermediate + final masks for analysis
-def create_mask_r_and_b_minus_h(rgb_paths, nir_paths, mask_paths, num_images):
+def create_mask_r_and_b_minus_h(rgb_paths, nir_paths, mask_paths, config):
     results = {
         "mask_r_and_nir": [],
         "mask_b_and_nir": [],
@@ -17,7 +17,7 @@ def create_mask_r_and_b_minus_h(rgb_paths, nir_paths, mask_paths, num_images):
     }
 
     for i, (rgb_path, nir_path, mask_path) in enumerate(zip(rgb_paths, nir_paths, mask_paths)):
-        if i >= num_images:
+        if i >= config["NUM_IMAGES"]:
             break
 
         rgb = imread(rgb_path)
@@ -31,34 +31,31 @@ def create_mask_r_and_b_minus_h(rgb_paths, nir_paths, mask_paths, num_images):
         H_chan = color.rgb2hsv(rgb)[:,:,0].astype(float)
         B = rgb[:,:,2].astype(float)
 
-        nir_threshold = NIR_resized.mean() - 0.5 * NIR_resized.std()
-        r_threshold = R.mean() + 0.1*R.std()
-        b_threshold = B.mean() + 0.1*B.std()
-        h_threshold = H_chan.mean() - 0.1*H_chan.std()
+        nir_threshold = NIR_resized.mean() - config["NIR_STD_MULT"] * NIR_resized.std()
+        r_threshold = R.mean() + config["R_STD_MULT"]*R.std()
+        b_threshold = B.mean() + config["B_STD_MULT"]*B.std()
+        h_threshold = H_chan.mean() - config["H_STD_MULT"]*H_chan.std()
 
-        mask_r_and_nir = clean_mask((R > r_threshold) & (NIR_resized < nir_threshold), minimum_size=5, selem_close=3)
-        mask_b_and_nir = clean_mask((B > b_threshold) & (NIR_resized < nir_threshold), minimum_size=5, selem_close=3)
-        mask_h_and_nir = clean_mask((H_chan < h_threshold) & (NIR_resized < nir_threshold), minimum_size=150, selem_close=2)
+        mask_r_and_nir = clean_mask((R > r_threshold) & (NIR_resized < nir_threshold), minimum_size=config["R_MIN_SIZE"], selem_close=config["R_SELEM_CLOSE"])
+        mask_b_and_nir = clean_mask((B > b_threshold) & (NIR_resized < nir_threshold), minimum_size=config["B_MIN_SIZE"], selem_close=config["B_SELEM_CLOSE"])
+        mask_h_and_nir = clean_mask((H_chan < h_threshold) & (NIR_resized < nir_threshold), minimum_size=config["H_MIN_SIZE"], selem_close=config["H_SELEM_CLOSE"])
 
         h_black_ratio = 1.0 - (mask_h_and_nir.sum() / mask_h_and_nir.size)
-        use_h = h_black_ratio < 0.9
+        use_h = h_black_ratio < config["H_BLACK_RATIO"]
 
         mask_r_b = mask_r_and_nir & mask_b_and_nir
         mask_r_b = dilation(mask_r_b, disk(2))
 
+        mask_final = mask_r_b & (~mask_h_and_nir)
+        mask_final = clean_mask(mask_final, minimum_size=config["MIN_SIZE_WEAK"], selem_close=config["SELEM_CLOSE"])
+        mask_final = remove_big_objects(mask_final, max_size=config["MAX_SIZE"])
+        
         if use_h:
-            mask_final = mask_r_b & (~mask_h_and_nir)
-            mask_final = clean_mask(mask_final, minimum_size=5, selem_close=3)
-            mask_final = remove_big_objects(mask_final, max_size=1200)
-            mask_final = clean_mask(mask_final, minimum_size=50, selem_close=3)
-
+            mask_final = clean_mask(mask_final, minimum_size=config["MIN_SIZE_STRONG"], selem_close=config["SELEM_CLOSE"])
         else:
-            mask_final = mask_r_b & (~mask_h_and_nir)
-            mask_final = clean_mask(mask_final, minimum_size=5, selem_close=3)
-            mask_final = remove_big_objects(mask_final, max_size=1200)
-            mask_final = clean_mask(mask_final, minimum_size=5, selem_close=3)
+            mask_final = clean_mask(mask_final, minimum_size=config["MIN_SIZE_WEAK"], selem_close=config["SELEM_CLOSE"])
 
-        mask_final, _, _ = adaptive_fp_cleanup(mask_final, mask_gt, fp_ratio_thr=0.4)
+        mask_final, _, _ = adaptive_fp_cleanup(mask_final, mask_gt, fp_ratio_thr=config["FP_RATIO_THR"])
 
         results["mask_r_and_nir"].append(mask_r_and_nir)
         results["mask_b_and_nir"].append(mask_b_and_nir)
